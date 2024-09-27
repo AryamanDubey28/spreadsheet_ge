190a52dca48644d391bb3b0f1f0a60b1a5825843
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2024-09-27 17:33:24 +0100

    allow backend to listen to the frontend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  #TODO:Verify this is the frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
